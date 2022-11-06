a3f0379262fc44f8bbe199e7c530beffd51d8e0d
Damir Arh <damir.arh@gmail.com>
2022-11-06 08:48:59 +0100

    Add project files.

---
+++ NullableTypesInAttributes/MyAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace NullableTypesInAttributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class MyAttribute : Attribute
+{
+    public MyAttribute() { }
+
+    public MyAttribute(int nullableParameter)
+    {
+        NullableParameter = nullableParameter;
+    }
+
+    public int? NullableParameter { get; set; }
+}
---
+++ NullableTypesInAttributes/Program.cs
@@ -0,0 +1,12 @@
+﻿using NullableTypesInAttributes;
+using System.Reflection;
+
+var attributes = typeof(MyRecord).GetCustomAttributes<MyAttribute>(true);
+foreach (var attribute in attributes)
+{
+    Console.WriteLine($"NullableParameter: {attribute.NullableParameter}");
+}
+
+[My()]
+[My(nullableParameter: 1)]
+public record MyRecord();
